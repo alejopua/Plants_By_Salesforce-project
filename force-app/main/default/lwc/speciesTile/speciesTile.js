125ad41b8a947038f4b9aad2740a29490d63064a
alejopua <alejopua@gmail.com>
2024-08-13 22:43:44 -0500

    chore: Update .forceignore and speciesTile component

--- force-app/main/default/lwc/speciesTile/speciesTile.js
+++ force-app/main/default/lwc/speciesTile/speciesTile.js
@@ -1,5 +1,26 @@
 import { api, LightningElement } from 'lwc';
+import { NavigationMixin } from 'lightning/navigation';
 
-export default class SpeciesTile extends LightningElement {
+export default class SpeciesTile extends NavigationMixin(LightningElement) {
   @api specie;
+
+  get isOutdoor() {
+    return this.specie.Location__c.includes('Outdoor')
+  };
+
+  get isIndoor() {
+    return this.specie.Location__c.includes('Indoor')
+  };
+
+  navigateToRecordViewPage() {
+    // View a custom object record.
+    this[NavigationMixin.Navigate]({
+      type: "standard__recordPage",
+      attributes: {
+        recordId: this.specie.Id,
+        objectApiName: "Species__c", // objectApiName is optional
+        actionName: "view",
+      },
+    });
+  }
 }
